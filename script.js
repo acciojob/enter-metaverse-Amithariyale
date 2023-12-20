14032af56b03b71f15a1da219c923943220adaad
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2023-12-20 22:14:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 //your JS code here. If required.
 
-function enterMetavese() {
+const btn=document.getElementById('enterBtn');
+btn.addEventListener('click',function enterMetavese() {
 	const element=document.getElementById("status");
 	
 	element.innerText="Entered Metaverse";
 	element.outerHTML=element.outerHTML.replace(/p/g,"h1");
-}
+})
